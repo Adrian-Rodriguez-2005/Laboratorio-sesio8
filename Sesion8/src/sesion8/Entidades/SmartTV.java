8199a62f71d843937ebf2f2fd57bb6347dfbc866
Adrian-Rodriguez-2005 <166246268+Adrian-Rodriguez-2005@users.noreply.github.com>
2024-05-18 13:58:54 -0500

    Add files via upload

---
+++ Sesion8/src/sesion8/Entidades/SmartTV.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sesion8.Entidades;
+
+import sesion8.Interfaces.Navegable;
+import sesion8.Interfaces.Reproducible;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class SmartTV implements Navegable,Reproducible{
+
+    @Override
+    public void navegar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void reproducir() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+}
